008c0b7d1c7b7a5ff82fe6ad41c38c5c1ea1d569
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-05-03 16:56:09 -0300

    Reducer composition

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,3 +1,22 @@
+const todo = (todo, action) => {
+    switch (action.type){
+        case('ADD_TODO'):
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case('TOGGLE_TODO'):
+            if (todo.id !== action.id){
+                return todo;
+            }
+            return {...todo,
+                completed: !todo.completed
+            };
+        default:
+            return todo;
+    }
+}
 const todos = (state = [], action) => {
     switch (action.type){
         case 'ADD_TODO':
@@ -5,12 +24,12 @@ const todos = (state = [], action) => {
         console.log('ADD_TODO');
             return [
                 ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
+                todo(undefined, action)
             ]
+        case 'TOGGLE_TODO':
+        console.log(state);
+        console.log('TOGGLE_TODO');
+            return state.map(t => todo(t, action));
         default:
             return state;
     }
